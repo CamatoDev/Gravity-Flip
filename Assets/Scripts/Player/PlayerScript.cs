c4a9bc49536b78001c84d9977aac1fbf679d018a
jordankamto <jordankamto32@gmail.com>
2025-05-14 20:50:28 +0100

    updated player movement system and added final assets for terrain

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -1,19 +1,56 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Animator playerAnimator;
+    float axisH, axisV;
+    [SerializeField]
+    float walkSpeed = 2f, runSpeed = 5f, rotationSpeed = 100f, jumpForce = 2f;
+
+    void Awake()
     {
-        Debug.Log("Hello world");
+        playerAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Debug.Log("Update");
+        axisH = Input.GetAxis("Horizontal");
+        axisV = Input.GetAxis("Vertical");
+
+        if (axisV > 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                transform.Translate(Vector3.forward * axisV * runSpeed * Time.deltaTime);
+                playerAnimator.SetFloat("run", axisV);
+            }
+            else
+            {
+                transform.Translate(Vector3.forward * axisV * walkSpeed * Time.deltaTime);
+                playerAnimator.SetBool("walk", true);
+                playerAnimator.SetFloat("run", 0);
+            }
+
+        }
+        else
+        {
+            playerAnimator.SetBool("walk", false);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            playerAnimator.SetTrigger("jump");
+        }
+
+        if (axisH != 0)
+        {
+            Debug.Log("AxisH " + axisH);
+            Debug.Log("Vector3.up " + Vector3.up);
+            Debug.Log("rotationSpeed " + rotationSpeed);
+            Debug.Log("Time.deltaTime " + Time.deltaTime);
+        }
+        transform.Rotate(Vector3.up * axisH * rotationSpeed * Time.deltaTime);
     }
 }
